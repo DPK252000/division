7ae28a1aab464a695eea679dfa636c94e959fc73
DPK252000 <113545795+DPK252000@users.noreply.github.com>
2022-09-30 17:00:23 +0530

    Add files via upload

---
+++ Division.py
@@ -0,0 +1,12 @@
+"""Method 1"""
+if__name__=='__main__':
+    a=int(input())
+    b=int(input())
+    print(a/b)
+    print(a//b)
+
+    """Method 2"""
+    a=12
+    b=6
+    print(a/b)
+    print(a//b)
